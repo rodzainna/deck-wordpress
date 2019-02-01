b7d6831294620ba5caee170e05940384bfba505a
JediMesa <jedidiah@mesasix.com>
2019-02-01 14:48:44 +0800

    init home templates

--- wp-content/themes/year-of-tolerance/footer.php
+++ wp-content/themes/year-of-tolerance/footer.php
@@ -2,33 +2,17 @@
 /**
  * The template for displaying the footer
  *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
  * @package year-of-tolerance
  */
 
 ?>
 
-	</div><!-- #content -->
+	</div>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'year-of-tolerance' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'year-of-tolerance' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'year-of-tolerance' ), 'year-of-tolerance', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer class="site-footer">
+		
+	</footer>
+</div><!-- #app -->
 
 <?php wp_footer(); ?>
 
